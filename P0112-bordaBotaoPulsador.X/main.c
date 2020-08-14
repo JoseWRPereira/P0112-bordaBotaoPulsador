b1644bc38482528a7b6f68596898af615a1a24dd
JoseWRPereira <josewrpereira@gmail.com>
2020-08-14 08:42:54 -0300

    Atualização de arquivos, detalhes, datas...

--- P0112-bordaBotaoPulsador.X/main.c
+++ P0112-bordaBotaoPulsador.X/main.c
@@ -2,12 +2,12 @@
  * File:   main.c
  * Author: josewrpereira
  *
- * Created on 13 de Agosto de 2020, 17:31:21
- * 
+ * Created on 13 de Agosto de 2020, 23:02:21
+ *
  * -------------------------------------------------
  *          MAPA DE ENTRADAS E SAIDAS
  * -------------------------------------------------
- *  Pinos   |n�     |Conex�o
+ *  Pinos   |nº     |Conexão
  * ---------|-------|-------------------------------
  *  RB0     |33     | Segmento a
  *  RB1     |34     | Segmento b
@@ -19,6 +19,10 @@
  *  RB7     |40     | Segmento p
  *  RD7     |30     | Hab. Dezena (Catodo comum)
  *  RD6     |29     | Hab. Unidade (Catodo comum)
+ * ---------|-------|-------------------------------
+ *  RD2     |21     | Botão -1
+ *  RD3     |22     | Botão +1
+ * -------------------------------------------------
  * -------------------------------------------------
  */
 
@@ -28,22 +32,22 @@
 #include "disp7seg.h"
 #include "botao.h"
 
-void main(void) 
+void main(void)
 {
     char num = 0;
-    
+
     disp7segX2_init();
     botao_init();
-    
+
     while( 1 )
     {
         disp7segX2( num );
-        
+
         if( b1_bordaSubida() )
             num = ++num % 100;
         if( b0_bordaDescida() )
             num = num==0 ? 99 : num-1;
-        
+
         delay(1);
     }
     return;
---
+++ P0112-bordaBotaoPulsador.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=P0112-bordaBotaoPulsador.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/P0112-bordaBotaoPulsador.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=p0112-bordabotaopulsador.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/p0112-bordabotaopulsador.x.tar
